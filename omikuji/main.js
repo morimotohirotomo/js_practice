aef834dc1e3daae18d08c66655788db7a806dce7
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-28 23:11:17 +0900

    finish

--- omikuji/main.js
+++ omikuji/main.js
@@ -3,9 +3,14 @@
 {
   const btn = document.getElementById('btn');
 
-  btn.addEventListener('click', () =>{
-    const results = ['大吉', '中吉', '凶', '末吉']
-  	const n = Math.floor(Math.random() * results.length);
-  	btn.textContent = results[n];
+  btn.addEventListener('click', () => {
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.2) {
+      btn.textContent = '吉';
+    } else {
+      btn.textContent = '凶';
+    }
   });
 }
