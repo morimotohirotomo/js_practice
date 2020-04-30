f65ab21009a38f9266f324512ef867d713c8fc13
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-30 19:10:47 +0900

    finish stopwatch

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -22,17 +22,46 @@
   	}, 10);
   }
 
+  function setButtonStateInitial() {
+  	start.classList.remove('inactive');
+  	stop.classList.add('inactive');
+  	reset.classList.add('inactive');
+  }
+  function setButtonStateRunning() {
+  	start.classList.add('inactive');
+  	stop.classList.remove('inactive');
+  	reset.classList.add('inactive');
+  }
+  function setButtonStateStoped() {
+  	start.classList.remove('inactive');
+  	stop.classList.add('inactive');
+  	reset.classList.remove('inactive');
+  }
+
+  setButtonStateInitial();
+
   start.addEventListener('click', () => {
+  	if (start.classList.contains('inactive') === true) {
+  	  return;
+  	}
+  	setButtonStateRunning();
   	startTime = Date.now();
   	countUp();
   });
 
   stop.addEventListener('click', () => {
+  	if (stop.classList.contains('inactive') === true) {
+  	  return;
+  	}
+  	setButtonStateStoped()
   	clearTimeout(timeoutId);
   	elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+  	if (reset.classList.contains('inactive') === true) {
+  	  return;
+  	}
   	timer.textContent = '00:00.000';
   	elapsedTime = 0;
   });
