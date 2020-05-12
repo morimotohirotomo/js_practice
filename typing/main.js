4cae47a4eb5443a82f93044b5249823b2f659872
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-12 16:05:45 +0900

    finish typing

--- typing/main.js
+++ typing/main.js
@@ -8,12 +8,13 @@
 		'middle',
 		'set',
 	];
-	let word = words[Math.floor(Math.random() * words.length)];
-	let loc = 0;
-	let score = 0;
-	let miss = 0;
+	let word;
+	let loc;
+	let score;
+	let miss;
 	const timeLimit = 3 * 1000;
 	let startTime;
+	let isPlaying = false;
 
 	const target = document.getElementById('target');
 	const scoreLabel = document.getElementById('score');
@@ -38,18 +39,43 @@
 		}, 10);
 
 		if (timeLeft < 0) {
+			isPlaying = false;
 			clearTimeout(timeoutId);
-			alert('Game Over');
+			timerLabel.textContent = '0.00';
+			setTimeout(() => {
+				showResult();
+			}, 100);
+
+			target.textContent = 'click to replay';
 		}
 	}
 
+	function showResult() {
+		const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+		alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy!`);
+	}
+
 	window.addEventListener('click', () => {
+		if (isPlaying === true) {
+			return;
+		}
+		isPlaying = true;
+
+		loc = 0;
+		score = 0;
+		miss = 0;
+		scoreLabel.textContent = score;
+		missLabel.textContent = miss;
+		word = words[Math.floor(Math.random() * words.length)];
 		target.textContent = word;
 		startTime = Date.now();
 		updateTimer();
 	});
 
 	window.addEventListener('keydown', e => {
+		if (isPlaying !== true) {
+			return;
+		}
 		console.log(e.key);
 		if (e.key === word[loc]) {
 			loc++;
