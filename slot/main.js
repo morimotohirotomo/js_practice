3bb352405cd0a34ba5c16d45f458d269648c8548
morimotohirotomo <minamoyori19@outlook.jp>
2020-05-24 14:13:43 +0900

    finish slot

--- slot/main.js
+++ slot/main.js
@@ -13,13 +13,19 @@
 
 			this.stop = document.createElement('div');
 			this.stop.textContent = 'STOP';
-			this.stop.classList.add('stop');
+			this.stop.classList.add('stop', 'inactive');
 			this.stop.addEventListener('click', () => {
+				if (this.stop.classList.contains('inactive')) {
+					return;
+				}
+				this.stop.classList.add('inactive');
 				clearTimeout(this.timeoutId);
 
 				panelsLeft--;
 
 				if (panelsLeft === 0) {
+					spin.classList.remove('inactive');
+					panelsLeft = 3;
 					checkResult();
 				}
 			});
@@ -54,6 +60,11 @@
 		unmatch() {
 			this.img.classList.add('unmatched');
 		}
+
+		activate() {
+			this.img.classList.remove('unmatched');
+			this.stop.classList.remove('inactive');
+		}
 	}
 
 	function checkResult() {
@@ -78,7 +89,12 @@
 
 	const spin = document.getElementById('spin');
 	spin.addEventListener('click', () => {
+		if (spin.classList.contains('inactive')) {
+			return;
+		}
+		spin.classList.add('inactive');
 		panels.forEach(panel => {
+			panel.activate();
 			panel.spin();
 		});
 	});
